b180e87a5c6299142f1c91916638f3a250bea2bc
Vachounet <vachounet@live.fr>
2019-03-20 14:16:57 +0100

    add support for AndroidFileHost search

--- bot.js
+++ bot.js
@@ -50,6 +50,11 @@ tg.router
     )
 
     .when(
+        new TextCommand('/afh search', 'afhSearchHandler', 'Search for files on AndroidFileHost'),
+        new AFHController()
+    )
+
+    .when(
         new CustomFilterCommand($ => {
             return $.message.text.indexOf("https://rsdsecure-cloud.motorola.com/download/") !== -1
         }, 'rsdFilterHandler'),
