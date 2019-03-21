897a8d18284b4deac7e0998cd09e65a43790a932
Vachounet <vachounet@live.fr>
2019-03-21 10:33:15 +0100

    add support for ViperOS

--- bot.js
+++ bot.js
@@ -189,6 +189,11 @@ tg.router
     )
 
     .when(
+        new TextCommand('/viper', 'viperBuildHandler', ''),
+        new exports["viper"]()
+    )
+
+    .when(
         new CustomFilterCommand($ => {
             return $.message.text.indexOf("https://rsdsecure-cloud.motorola.com/download/") !== -1
         }, 'rsdFilterHandler'),
