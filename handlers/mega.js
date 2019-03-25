55dac3b24f7184f171136126393f307e20c91a75
Vachounet <vachounet@live.fr>
2019-03-25 09:54:49 +0100

    mega: don't send button if private chat

--- handlers/mega.js
+++ handlers/mega.js
@@ -1,6 +1,6 @@
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController;
-
+var exec = require('child_process').exec;
 
 class MegaController extends TelegramBaseController {
 
@@ -12,17 +12,31 @@ class MegaController extends TelegramBaseController {
             inline_keyboard: []
         };
 
-        kb.inline_keyboard.push(
+        if ($.message.chat.type === "private") {
+            exec(__dirname + "/../megadown '" + matches[0] + "'", function callback(error, stdout, stderr) {
+                var json = JSON.parse(stdout);
+
+                tg.api.sendMessage($.message.from.id, "*Download Link* :\n[" + json.file_name + "](" + json.url + ")", {
+                    parse_mode: "markdown",
+                    reply_markup: JSON.stringify(kb)
+                });
+
+
+            });
+        } else {
+
+            kb.inline_keyboard.push(
                     [{
-                text: "Click here to get it throught PM",
-                callback_data: "mega|" + matches[0].split("#")[1]
+                    text: "Click here to get it throught PM",
+                    callback_data: "mega|" + matches[0].split("#")[1]
                     }]);
 
-        $.sendMessage("*Generate download link* ", {
-            parse_mode: "markdown",
-            reply_markup: JSON.stringify(kb),
-            reply_to_message_id: $.message.messageId
-        });
+            $.sendMessage("*Generate download link* ", {
+                parse_mode: "markdown",
+                reply_markup: JSON.stringify(kb),
+                reply_to_message_id: $.message.messageId
+            });
+        }
 
     }
 
